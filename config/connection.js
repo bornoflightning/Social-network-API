cf1c7a2a21804e084f9e09d40ed89757e7140fc0
bornoflightning <victor.escarcega@hotmail.com>
2023-02-04 19:27:42 -0700

    updated connection name

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialNetworkDB';
+const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network-api';
 
 connect(connectionString, {
   useNewUrlParser: true,
